2b1746c847551c893441329e8a1b12b57dc26d0d
m-boswell <33388669+m-boswell@users.noreply.github.com>
2023-10-29 10:43:29 -0400

    Update FileAsset.java

--- src/main/java/org/example/FileAsset.java
+++ src/main/java/org/example/FileAsset.java
@@ -1,20 +1,50 @@
-package org.example;
 
-import java.io.File;
 import java.util.Date;
 
-public class FileAsset {
-    File file;
+public class Asset {
+    private String name;
+    private String path;
+    private Date date;
 
-    public FileAsset(File file) {
-        this.file = file;
+    // Constructor
+    public Asset(String name, String path, Date date) {
+        this.name = name;
+        this.path = path;
+        this.date = date;
     }
 
-    public File getFile() {
-        return file;
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getPath() {
+        return path;
     }
 
     public Date getDate() {
-        return new Date(file.lastModified());
+        return date;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Asset{" +
+                "name='" + name + '\'' +
+                ", path='" + path + '\'' +
+                ", date=" + date +
+                '}';
     }
 }
